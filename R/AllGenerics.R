a4ec6b95ea9da2227162dd4d55b43cef863021fc
Lluís Revilla <lluis.revilla@gmail.com>
2018-12-02 17:10:33 +0100

    Add elements method

---
+++ R/AllGenerics.R
@@ -0,0 +1,9 @@
+
+#' Retrieve the elements
+#'
+#' Given a Set or a SetCollection retrieve the elements
+#' @param object Either a \code{Set} or a \code{SetCollection} object
+#' @export
+setGeneric("elements", function(object)
+  standardGeneric("elements")
+)
