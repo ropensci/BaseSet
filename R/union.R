d42101a8047dde1a859ed52a05c8f27e392c6f1f
llrs <lluis.revilla@gmail.com>
2019-01-30 17:56:00 +0100

    Introduce the union method

--- R/union.R
+++ R/union.R
@@ -1,7 +1,7 @@
 #' @include AllClasses.R AllGenerics.R
 NULL
 
-#' @describeIn union Merge two sets
+#' @describeIn union Applies the standard union
 #' @export
 setMethod("union",
           signature = signature(object = "TidySet",
@@ -19,6 +19,20 @@ setMethod("union",
             levels(object@relations$sets)[sets2 %in% set1] <- setName
             levels(object@relations$sets)[sets2 %in% set2] <- setName
 
+            # Handle the duplicate cases
+            basic <- paste(object@relations$sets, object@relations$elements)
+            indices <- split(seq_along(basic), basic)
+            # Helper function probably useful for intersection too
+            iterate <- function(i, fuzzy, fun) {
+              fun(fuzzy[i])
+            }
+            # It could be possible to apply some other function to the relations
+            # that are the same
+            fuzzy <- vapply(indices, iterate, fuzzy = object@relations$fuzzy,
+                            fun = max, numeric(1L))
+            relations2 <- unique(object@relations[, c("sets", "elements")])
+            relations2 <- cbind.data.frame(relations2, fuzzy = fuzzy)
+            object@relations <- relations2
             validObject(object)
             object
           }
--- tests/testthat/test-union.R
+++ tests/testthat/test-union.R
@@ -6,4 +6,22 @@ test_that("union works", {
   a <- tidySet(relations)
   b <- union(a, "c", "b", "d") # Simple case without merging fuzzy (just renaming)
   expect_s4_class(b, "TidySet")
+  expect_equal(name_sets(b), c("a", "d"))
+
+  # Simple case with duplicate relations
+  relations <- data.frame(sets = c(rep("a", 5), "b", "c"),
+                          elements = c(letters[seq_len(6)], letters[6]))
+  a <- tidySet(relations)
+  b <- union(a, "c", "b", "d")
+  expect_s4_class(b, "TidySet")
+})
+
+test_that("union works", {
+
+  relations <- data.frame(sets = c(rep("a", 5), "b", "c"),
+                          elements = c(letters[seq_len(6)], letters[6]),
+                          fuzzy = runif(7))
+  a <- tidySet(relations)
+  b <- union(a, "c", "b", "d") # Simple case without merging fuzzy (just renaming)
+  expect_s4_class(b, "TidySet")
 })
