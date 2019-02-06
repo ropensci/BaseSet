a29fc9c3de8fab44a3cff0777b495126295ec26f
llrs <lluis.revilla@gmail.com>
2019-02-06 16:19:56 +0100

    Add an argument to include the fuzzy column

--- R/add_set.R
+++ R/add_set.R
@@ -7,16 +7,19 @@ setMethod("add_set",
           signature = signature(object = "TidySet",
                                 elements = "character",
                                 setName = "character"),
-          function(object, elements, setName) {
+          function(object, elements, setName, fuzzy = 1) {
             if (length(setName) != 1) {
               stop("The new set should have one name")
+            }
+            if (!is.numeric(fuzzy)) {
+              stop("fuzzy should be numeric between 0 and 1")
             }
             object <- add_sets(object, setName)
             object <- add_elements(object, elements)
             object <- add_relations(object,
                                     elements = elements,
                                     sets = setName,
-                                    fuzzy = 1)
+                                    fuzzy = fuzzy)
             validObject(object)
             object
           }
