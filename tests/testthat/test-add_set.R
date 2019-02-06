b3c94f316882a5b48aa21fbe2786b31a3e651b1b
llrs <lluis.revilla@gmail.com>
2019-02-06 17:58:51 +0100

    Reorganize and add tests

--- tests/testthat/test-add_set.R
+++ tests/testthat/test-add_set.R
@@ -9,10 +9,25 @@ test_that("add_set works", {
   expect_equal(nSets(b), nSets(a) + 1)
 
   expect_error(add_set(a, elements = letters[7:15], setName = c("ha", "he")))
-  expect_error(add_set(a, elements = letters[7:15], setName = c("ha", "he"), fuzzy = -1))
 
   elements(a) <- cbind(elements(a), Ha = c("bla", "ble", "bli", "blo", "blu", NA))
   d <- add_set(a, elements = letters[1:2], setName = "DF2")
   expect_equal(nSets(d), nSets(a) + 1)
   expect_error(add_set(a, elements = letters[1:2], setName = c("DF2", "df2")))
+})
+
+test_that("add_set fuzzy works", {
+  relations <- data.frame(sets = c(rep("A", 5), "B", "B"),
+                          elements = c("a", "b", "c", "d", "e", "a", "f"))
+  a <- tidySet(relations)
+  b <- add_set(a, elements = letters[7:15], setName = "ha", fuzzy = 0.5)
+  expect_s4_class(b, "TidySet")
+  expect_equal(nSets(b), nSets(a) + 1)
+
+  elements(a) <- cbind(elements(a), Ha = c("bla", "ble", "bli", "blo", "blu", NA))
+  d <- add_set(a, elements = letters[7:15], setName = "ha", fuzzy = 0.5)
+  expect_s4_class(d, "TidySet")
+  expect_equal(nSets(d), nSets(a) + 1)
+  expect_error(add_set(a, elements = letters[7:15], setName = "ha", fuzzy = -1))
+  expect_error(add_set(a, elements = letters[7:15], setName = c("ha", "he"), fuzzy = 1))
 })
