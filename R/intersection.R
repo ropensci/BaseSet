d58c32e6c17bb003ecdd2bdcb13b36a6e6967dda
llrs <lluis.revilla@gmail.com>
2019-02-07 16:16:22 +0100

    Reindent intersection function

--- R/intersection.R
+++ R/intersection.R
@@ -11,9 +11,9 @@ setMethod("intersection",
           function(object, set1, set2, setName, FUN = "min", keep = FALSE) {
             object <- operation_helper(object, set1, set2, setName, FUN, keep)
             if (!keep) {
-            sets <- name_sets(object)
-            remove <- sets[!sets %in% setName]
-            object <- remove_set(object, remove)
+              sets <- name_sets(object)
+              remove <- sets[!sets %in% setName]
+              object <- remove_set(object, remove)
             }
             object
           }
