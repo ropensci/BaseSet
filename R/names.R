6628d0c71fb2830c0e06be9e6806e21c3c206c4a
llrs <lluis.revilla@gmail.com>
2019-02-07 15:45:24 +0100

    Testing What happens with duplicate levels/info

--- R/names.R
+++ R/names.R
@@ -25,9 +25,13 @@ setMethod("name_elements<-",
           signature = signature(object = "TidySet", value = "character"),
           function(object, value){
             old <- levels(object@elements$elements)
-
-            levels(object@elements$elements) <- value
+            value2 <- rep(value, length.out = length(name_elements(object)))
+            levels(object@elements$elements) <- value2
             object@elements <- unique(object@elements)
+            if (anyDuplicated(object@elements$element)) {
+              stop("Duplicated elements but with different information")
+            }
+
             old_relations <- levels(object@relations$elements)
             replace <- match(old_relations, old)
             levels(object@relations$elements)[replace] <- value
@@ -41,9 +45,12 @@ setMethod("name_sets<-",
           signature = signature(object = "TidySet", value = "character"),
           function(object, value){
             old <- levels(object@sets$set)
-
-            levels(object@sets$set) <- value
+            value2 <- rep(value, length.out = length(name_sets(object)))
+            levels(object@sets$set) <- value2
             object@sets <- unique(object@sets)
+            if (anyDuplicated(object@sets$set)) {
+              stop("Duplicated sets but with different information")
+            }
             old_relations <- levels(object@relations$sets)
             replace <- match(old_relations, old)
             levels(object@relations$sets)[replace] <- value
--- tests/testthat/test-name.R
+++ tests/testthat/test-name.R
@@ -18,6 +18,8 @@ test_that("name_sets<- works", {
   name_sets(a) <- LETTERS[1:2]
   out <- name_sets(a)
   expect_equal(out, LETTERS[1:2])
+  b <- add_column(a, "sets", data.frame(Ha = c("a", "b")))
+  expect_error(name_sets(b) <- "A2", "Duplicated sets")
 })
 
 test_that("name_elements works", {
@@ -29,6 +31,7 @@ test_that("name_elements works", {
   expect_length(out, 6L)
   expect_equal(out, letters[1:6])
 })
+
 test_that("name_elements<- works", {
   relations <- data.frame(sets = c(rep("a", 5), "b"),
                          elements = letters[seq_len(6)],
@@ -38,4 +41,7 @@ test_that("name_elements<- works", {
   out <- name_elements(a)
   expect_length(out, 6L)
   expect_equal(out, LETTERS[1:6])
+
+  b <- add_column(a, "elements", data.frame(Ha = paste0(letters[1:6], "2")))
+  expect_error(name_elements(b) <- "A2", "Duplicated elements")
 })
