388dd7faa2941cb26653c0a1ae75c935e580f0da
llrs <lluis.revilla@gmail.com>
2019-02-08 16:06:52 +0100

    Remove te operation_helper from the implementation of intersection

--- R/intersection.R
+++ R/intersection.R
@@ -9,12 +9,33 @@ setMethod("intersection",
                                 set2 = "character",
                                 setName = "character"),
           function(object, set1, set2, setName, FUN = "min", keep = FALSE) {
-            object <- operation_helper(object, set1, set2, setName, FUN, keep)
-            if (!keep) {
-              sets <- name_sets(object)
-              remove <- sets[!sets %in% setName]
-              object <- remove_set(object, remove)
+
+            sets <- c(set1, set2)
+            if (length(set1) != length(setName)) {
+              stop("New names must be of the same length as the pairs of ",
+                   "set to unite", call. = FALSE)
             }
-            object
+            new_object <- rename_set(object, sets, setName)
+            if (!keep) {
+              old_sets <- name_sets(object)
+              remove_sets <- old_sets[!old_sets %in% setName]
+              new_object <- remove_sets(new_object, remove_sets)
+              new_object <- rm_relations_with_sets(new_object, remove_sets)
+
+              remove_elements <- object %e-e% new_object
+              new_elements <- as.character(new_object@relations$elements)
+              duplicated <- new_elements[duplicated(new_elements)]
+              remove_too <- setdiff(new_elements, duplicated)
+              remove_elements <- c(remove_elements, remove_too)
+
+              new_object <- remove_elements(new_object, remove_elements)
+              new_object <- rm_relations_with_elements(new_object,
+                                                       remove_elements)
+            } else {
+              new_object <- merge_tidySets(object, new_object)
+            }
+
+            new_object <- fapply(new_object, FUN)
+            new_object
           }
 )
