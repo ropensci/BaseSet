ffcbabe769c32c163c009ada428690449840db56
llrs <lluis.revilla@gmail.com>
2019-02-21 12:44:09 +0100

    Import a list of symbols related to sets

---
+++ R/zzz.R
@@ -0,0 +1,22 @@
+
+#' A subset of symbols related to sets
+#'
+#' Name and symbol of operations related to sets:
+#' @references \url{https://www.fileformat.info/info/unicode/category/Sm/list.htm}
+#' @export
+#' @examples
+#' set_symbols
+set_symbols <- c("intersection" = "\u2229",
+             "union" = "\U222A",
+             "complement" = "\u2201",
+             "negation" = "\u00AC",
+             "subsetOrEqual" = "\u2286",
+             "supersetOrEqual" = "\u2287",
+             "notSubset" = "\u2284",
+             "notSuperset" = "\u2285",
+             "element" = "\u2208",
+             "notElement" = "\u2209",
+             "empty" = "\u2205",
+             "contains" = "\u220B",
+             "notContained" = "\u220C",
+             "minus" = "\u2216")
