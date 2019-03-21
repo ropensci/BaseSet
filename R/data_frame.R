4def5d24f2c335b3e2232007216f84bcf877be16
llrs <lluis.revilla@gmail.com>
2019-03-21 11:44:04 +0100

    Allowing to convert back data.frames back to TidySets

--- R/data_frame.R
+++ R/data_frame.R
@@ -2,20 +2,22 @@
 NULL
 
 setAs("TidySet", "data.frame", function(from) {
-  s <- merge(from@relations, from@sets,
-             by.x = "sets", by.y = "sets", sort = FALSE,
-             all.x = TRUE, suffix = c(".relations", ".sets"))
-  o <- merge(s, from@elements,
-             by.x = "elements", by.y = "elements", sort = FALSE,
-             all.x = TRUE, suffix = c("", ".elements"))
+    s <- merge(from@relations, from@sets,
+               by.x = "sets", by.y = "sets", sort = FALSE,
+               all.x = TRUE, suffix = c(".relations", ".sets")
+    )
+    o <- merge(s, from@elements,
+               by.x = "elements", by.y = "elements", sort = FALSE,
+               all.x = TRUE, suffix = c("", ".elements")
+    )
 
-  # Don't show the fuzzy column if they are all fuzzy
-  # Could confuse users when they see this column appear in relations
-  if (!is.fuzzy(from)) {
-    keep <- setdiff(colnames(o), "fuzzy")
-    o <- o[, keep]
-  }
-  o
+    # Don't show the fuzzy column if they are all fuzzy
+    # Could confuse users when they see this column appear in relations
+    if (!is.fuzzy(from)) {
+        keep <- setdiff(colnames(o), "fuzzy")
+        o <- o[, keep]
+    }
+    o
 })
 
 
@@ -28,7 +30,7 @@ setAs("TidySet", "data.frame", function(from) {
 #' @method as.data.frame TidySet
 #' @export
 as.data.frame.TidySet <- function(x, ...) {
-  as(x, "data.frame")
+    as(x, "data.frame")
 }
 
 
@@ -40,29 +42,34 @@ as.data.frame.TidySet <- function(x, ...) {
 #' @param df The flattened data.frame
 #' @return A TidySet object
 #' @noRd
-df2TS <- function(.data = NULL, df){
-  if (!is.null(.data)) {
-    colnames_sets <- colnames(sets(.data))
-    colnames_elements <- colnames(elements(.data))
-    colnames_relations <- colnames(relations(.data))
-  } else {
-    colnames_sets <- c("sets")
-    colnames_elements <- c("elements")
-    colnames_relations <- c("elements", "sets", "fuzzy")
-  }
-  final_colnames <- colnames(df)
-  colnames_data <- c(colnames_sets, colnames_elements, colnames_relations)
-  new_colnames <- setdiff(final_colnames, colnames_data)
-  relations <- df[, c(colnames_relations, new_colnames)]
+df2TS <- function(.data = NULL, df) {
+    if (!is.null(.data)) {
+        colnames_sets <- colnames(sets(.data))
+        colnames_elements <- colnames(elements(.data))
+        colnames_relations <- colnames(relations(.data))
+    } else {
+        colnames_sets <- c("sets")
+        colnames_elements <- c("elements")
+        colnames_relations <- c("elements", "sets", "fuzzy")
+    }
+    if (!"fuzzy" %in% colnames(df)) {
+        df$fuzzy <- 1
+    }
+    final_colnames <- colnames(df)
+    colnames_data <- c(colnames_sets, colnames_elements, colnames_relations)
+    new_colnames <- setdiff(final_colnames, colnames_data)
+    relations <- df[, c(colnames_relations, new_colnames)]
 
-  TS <- tidySet(relations)
+    TS <- tidySet(relations)
 
-  TS@elements <- merge(TS@elements,
-                       unique(df[, colnames_elements, drop = FALSE]),
-                       all.x = TRUE, all.y = FALSE, sort = FALSE)
-  TS@sets <- merge(TS@sets,
-                   unique(df[, colnames_sets, drop = FALSE]),
-                   all.x = TRUE, all.y = FALSE, sort = FALSE)
-  validObject(TS)
-  TS
+    TS@elements <- merge(TS@elements,
+                         unique(df[, colnames_elements, drop = FALSE]),
+                         all.x = TRUE, all.y = FALSE, sort = FALSE
+    )
+    TS@sets <- merge(TS@sets,
+                     unique(df[, colnames_sets, drop = FALSE]),
+                     all.x = TRUE, all.y = FALSE, sort = FALSE
+    )
+    validObject(TS)
+    TS
 }
