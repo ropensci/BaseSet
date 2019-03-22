9bbce9f287345508891b79ee1fce81f6fe1ac35d
llrs <lluis.revilla@gmail.com>
2019-03-22 09:39:52 +0100

    Document the missing name method

--- R/union.R
+++ R/union.R
@@ -46,7 +46,7 @@ setMethod("union",
           }
 )
 
-#' @describeIn union Applies the standard union
+#' @describeIn union Applies the standard union provding an automatic name
 #' @export
 setMethod("union",
           signature = signature(object = "TidySet",
