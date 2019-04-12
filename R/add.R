df46c4ae86e154ecb9101b9f707b44a95d879cba
llrs <lluis.revilla@gmail.com>
2019-04-12 16:39:31 +0200

    Add a way to include the fuzzy value

--- R/add.R
+++ R/add.R
@@ -49,7 +49,9 @@ add_relations_interal <- function(object, elements, sets, fuzzy) {
     if (length(sets) != nElements && length(sets) == 1) {
         sets <- rep(sets, nElements)
     } else if (length(sets) != nElements && length(sets) > 1) {
-        stop("Recycling sets is not allowed", call. = FALSE)
+        stop("The number of elements is greater than the number of sets.",
+             "It can be either equal to the number of sets or just one set"
+             , call. = FALSE)
     }
 
     original_relations <- elements_sets(object)
@@ -72,13 +74,15 @@ add_relations_interal <- function(object, elements, sets, fuzzy) {
         elements <- vapply(elements_sets, "[", i = 1, character(1L))
         sets <- vapply(elements_sets, "[", i = 2, character(1L))
 
-        df_relations <- data.frame(elements = elements, sets = sets, fuzzy = fuzzy)
+        df_relations <- data.frame(elements = elements,
+                                   sets = sets,
+                                   fuzzy = fuzzy)
         column_names <- setdiff(colnames(object@relations),
                                 c("sets", "elements", "fuzzy"))
         if (length(column_names) > 0) {
             df_relations[, column_names] <- NA
-            object@relations <- rbind.data.frame(object@relations, df_relations)
         }
+        object@relations <- rbind.data.frame(object@relations, df_relations)
     }
     rownames(object@relations) <- NULL
     object@relations <- droplevels(object@relations)
@@ -154,24 +158,44 @@ add_sets.TidySet <- function(object, sets) {
 #' Remember to add/modify them if needed with [`mutate`] or [`mutate_relation`]
 #' @inheritParams add_sets
 #' @inheritParams add_elements
+#' @param fuzzy The strength of the membership.
 #' @return A [`TidySet`] object with the new relations.
 #' @family add_*
 #' @examples
 #' x <- list("a" = letters[1:5], "b" = LETTERS[3:7])
 #' a <- tidySet(x)
 #' add_relations(a, elements = c("a", "b", "g"), sets = "d")
+#' add_relations(a, elements = c("a", "b"), sets = c("d", "g"))
+#' add_relations(a, elements = c("a", "b"), sets = c("d", "g"), fuzzy = 0.5)
+#' add_relations(a, elements = c("a", "b"), sets = c("d", "g"),
+#'               fuzzy = c(0.5, 0.7))
+
 #' @export
-add_relations <- function(object, elements, sets) {
+add_relations <- function(object, elements, sets, ...) {
     UseMethod("add_relations")
 }
 
 #' @export
 #' @method add_relations TidySet
-add_relations.TidySet <- function(object, elements, sets) {
-    browser()
+add_relations.TidySet <- function(object, elements, sets, fuzzy = 1) {
+
     object <- add_elements(object, elements)
     object <- add_sets(object, sets)
     object <- add_relations_interal(object, elements, sets, 1)
+
+    if (length(fuzzy) != length(elements) && length(fuzzy) != 1) {
+        stop("Fuzzy values do not match with the number of relations",
+             call. = FALSE)
+    }
+
+    relations <- relations(object)
+    e_s <- paste(relations$elements, relations$sets)
+    eo_so <- paste(elements, sets)
+    m <- match(e_s, eo_so)
+    m <- m[!is.na(m)]
+    relations$fuzzy[relations$elements %in% elements &
+                        relations$sets %in% sets] <- fuzzy[m]
+    relations(object) <- relations
     validObject(object)
     object
 }
