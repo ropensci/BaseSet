6867f86864a15d85d111486b6684e81c9c72189b
llrs <lluis.revilla@gmail.com>
2019-04-15 16:48:37 +0200

    Transform union into an S3 method

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -269,33 +269,6 @@ setGeneric("add_column", function(object, slot, columns)
 setGeneric("remove_column", function(object, slot, column_names)
   standardGeneric("remove_column"))
 
-#' Merge two sets
-#'
-#' Given a TidySets merges two sets into the new one.
-#' @param object A TidySet object
-#' @param sets The name of the sets to be used.
-#' @param name The name of the new set.
-#' @param FUN A function to be applied when performing the union.
-#' The standard union is the "max" function, but you can provide any other
-#' function that given a numeric vector returns a single number.
-#' @param keep A logical value if you want to keep
-#' @param keep_relations A logical value if you wan to keep old relations
-#' @param keep_elements A logical value if you wan to keep old elements
-#' @param keep_sets A logical value if you wan to keep old sets
-#' @param ... Other arguments.
-#' @return A \code{TidySet} object.
-#' @export
-#' @family methods that create new sets
-#' @family methods
-#' @examples
-#' relations <- data.frame(sets = c(rep("a", 5), "b"),
-#'                         elements = letters[seq_len(6)],
-#'                         fuzzy = runif(6))
-#' a <- tidySet(relations)
-#' union(a, c("a", "b"), "C")
-setGeneric("union", function(object, sets, name, ...)
-  standardGeneric("union"))
-
 #' Intersection of two sets
 #'
 #' Given a TidySets creates a new set with the elements on the both of them.
--- R/union.R
+++ R/union.R
@@ -1,65 +1,69 @@
 #' @include AllClasses.R AllGenerics.R operations.R
 NULL
 
-# To avoid conflicts with the dplyr package
+
+
+#' Merge two sets
+#'
+#' Given a TidySets merges two sets into the new one.
+#' @param object A TidySet object
+#' @param sets The name of the sets to be used.
+#' @param name The name of the new set.
+#' @param FUN A function to be applied when performing the union.
+#' The standard union is the "max" function, but you can provide any other
+#' function that given a numeric vector returns a single number.
+#' @param keep A logical value if you want to keep
+#' @param keep_relations A logical value if you wan to keep old relations
+#' @param keep_elements A logical value if you wan to keep old elements
+#' @param keep_sets A logical value if you wan to keep old sets
+#' @param ... Other arguments.
+#' @return A \code{TidySet} object.
 #' @export
-#' @method union TidySet
-union.TidySet <- function(...) {
-  # Check if first argument is a TidySet
-  if (is(..1, "TidySet")) {
-    union(...)
-  } else {
-    NextMethod()
-  }
+#' @family methods that create new sets
+#' @family methods
+#' @examples
+#' relations <- data.frame(sets = c(rep("a", 5), "b"),
+#'                         elements = letters[seq_len(6)],
+#'                         fuzzy = runif(6))
+#' a <- tidySet(relations)
+#' union(a, c("a", "b"), "C")
+union <- function(object, ...) {
+    UseMethod("union")
 }
 
-#' @describeIn union Applies the standard union
+# #' @export
+# union.default <- function(object, ...) {
+#     stopifnot(length(list(...)) == 1)
+#     base::union(object, ...)
+# }
+
+#' @rdname union
 #' @export
-setMethod("union",
-          signature = signature(object = "TidySet",
-                                sets = "characterORfactor",
-                                name = "characterORfactor"),
-          function(object, sets, name = NULL, FUN = "max", keep = FALSE,
-                   keep_relations = keep,
-                   keep_elements = keep,
-                   keep_sets = keep) {
-            if (length(name) != 1) {
-              stop("The new union can only have one name", call. = FALSE)
-            }
+#' @method union TidySet
+union.TidySet <- function(object, sets, name = NULL, FUN = "max", keep = FALSE,
+                          keep_relations = keep,
+                          keep_elements = keep,
+                          keep_sets = keep, ...) {
+    if (is.null(name)) {
+        name <- naming(sets1 = sets)
+    } else if (length(name) != 1) {
+        stop("The new union can only have one name", call. = FALSE)
+    }
 
-            new_object <- rename_set(object, sets, name)
-            if (!keep_relations) {
-              old_sets <- name_sets(object)
-              remove_sets <- old_sets[!old_sets %in% name]
-              new_object <- remove_sets(new_object, remove_sets)
-              new_object <- rm_relations_with_sets(new_object, remove_sets)
-              new_object <- remove_elements(new_object, object %e-e% new_object)
-            } else {
-              new_object <- merge_tidySets(object, new_object)
-            }
-            relations <- fapply(new_object@relations, FUN)
-            new_object@relations <- relations
+    new_object <- rename_set(object, sets, name)
+    if (!keep_relations) {
+        old_sets <- name_sets(object)
+        remove_sets <- old_sets[!old_sets %in% name]
+        new_object <- remove_sets(new_object, remove_sets)
+        new_object <- rm_relations_with_sets(new_object, remove_sets)
+        new_object <- remove_elements(new_object, object %e-e% new_object)
+    } else {
+        new_object <- merge_tidySets(object, new_object)
+    }
+    relations <- fapply(new_object@relations, FUN)
+    new_object@relations <- relations
 
-            new_object <- droplevels(new_object, !keep_elements, !keep_sets)
-            validObject(new_object)
-            new_object
-          }
-)
-
-#' @describeIn union Applies the standard union providing an automatic name
-#' @export
-setMethod("union",
-          signature = signature(object = "TidySet",
-                                sets = "characterORfactor",
-                                name = "missing"),
-          function(object, sets, FUN = "max", keep = FALSE,
-                   keep_relations = keep,
-                   keep_elements = keep,
-                   keep_sets = keep) {
-              name <- naming(sets1 = sets)
-              union(object, sets = sets, name = name, FUN = FUN, keep = keep,
-                         keep_relations = keep_relations,
-                         keep_elements = keep_elements,
-                         keep_sets = keep_sets)
-          }
-)
+    new_object <- droplevels(new_object, !keep_elements, !keep_sets)
+    validObject(new_object)
+    new_object
+}
