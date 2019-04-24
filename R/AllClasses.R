776243a3e26a18445f1aa083733b136fb70829d5
llrs <lluis.revilla@gmail.com>
2019-04-24 15:17:30 +0200

    Check unique relationship only if there are relationships

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -91,11 +91,15 @@ is.valid <- function(object) {
   }
 
   # Check that relations are unique
-  if (anyDuplicated(slot(object, "relations")[, c("elements", "sets")]) >= 1) {
-    errors <- c(errors,
-                "A relationship between an element and a set must be unique."
-    )
+  relations <- slot(object, "relations")
+  if (nrow(relations) > 0) {
+    if (anyDuplicated(relations[, c("elements", "sets")]) >= 1) {
+      errors <- c(errors,
+                  "A relationship between an element and a set must be unique."
+      )
+    }
   }
+
   if (length(errors) == 0) {
     TRUE
   } else {
