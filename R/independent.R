e34224cd874be0e590333151668b8fc4ba434705
llrs <lluis.revilla@gmail.com>
2019-04-25 15:50:06 +0200

    Add test to independent method

--- R/independent.R
+++ R/independent.R
@@ -4,7 +4,7 @@
 #' Checks if the elements of the sets are present in more than one set.
 #' @param object A [`TidySet`] object.
 #' @param sets A character vector with the names of the sets to analyze.
-#' @return A logical value indicating if the sets are independent or not.
+#' @return A logical value indicating if the sets are independent (TRUE) or not.
 #' @export
 #' @examples
 #' x <- list("A" = letters[1:5], "B" = letters[3:7], "C" = letters[6:10])
@@ -12,6 +12,7 @@
 #' independent(TS)
 #' independent(TS, c("A", "B"))
 #' independent(TS, c("A", "C"))
+#' independent(TS, c("B", "C"))
 independent <- function(object, sets) {
     UseMethod("independent")
 }
