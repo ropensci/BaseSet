1f09f4a2e37f0365f8300edaab59caa98bc811b5
llrs <lluis.revilla@gmail.com>
2019-06-03 16:30:57 +0200

    Some methods

--- R/head.R
+++ R/head.R
@@ -13,9 +13,30 @@ tail.TidySet <- function(x, n = 6L, ...) {
     tail(as(x, "data.frame"), n = n)
 }
 
-#' @importFrom base dim
 #' @export
-#' @method base TidySet
+#' @method dim TidySet
 dim.TidySet <- function(x) {
     c(nElements(x), nRelations(x), nSets(x))
+}
+
+#' @export
+sample <- function(x, size, replace = FALSE, prob = NULL) {
+    UseMethod("sample")
+}
+
+#' @export
+sample.default <- base::sample
+
+#' @export
+#' @method sample TidySet
+sample.TidySet <- function(x, size, replace = FALSE, prob = NULL) {
+    if (!is.numeric(size) | (size > nRelations(x) & !replace)) {
+        stop("cannot take a sample larger than the number of ",
+             "relations when replace  = FALSE")
+    }
+    relations <- relations(x)
+    y <- seq_len(nrow(relations))
+    out <- sample(y, size, replace, prob)
+
+    tidySet(relations[out, ])
 }
