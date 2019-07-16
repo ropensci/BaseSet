36f12017fd0d1b92af30ab68de782fd1412a4946
llrs <lluis.revilla@gmail.com>
2019-07-16 16:58:04 +0200

    Add example to the group method

--- R/group.R
+++ R/group.R
@@ -9,6 +9,10 @@ NULL
 #' @return A TidySet object with the new set
 #' @family methods
 #' @export
+#' @examples
+#' x <- list("a" = c("A" = 0.1, "B" = 0.5), "b" = c("A" = 0.2, "B" = 1))
+#' a <- tidySet(x)
+#' b <- group(a, "c", elements %in% c("A", "B"))
 group <- function(object, name, ...) {
   UseMethod("group")
 }
