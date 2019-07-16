163cedd24683add4f0b1695824faaab90b4eea45
llrs <lluis.revilla@gmail.com>
2019-07-16 16:52:43 +0200

    Change indentation

--- R/move_to.R
+++ R/move_to.R
@@ -9,28 +9,29 @@ setMethod("move_to",
                                 to = "characterORfactor",
                                 columns = "character"),
           function(object, from, to, columns) {
-            if (from == to) {
-              return(object)
-            }
+              if (from == to) {
+                  return(object)
+              }
 
-            from <- match.arg(from, c("sets", "elements", "relations"))
-            to <- match.arg(to, c("sets", "elements", "relations"))
-            from_df <- slot(object, from)
-            to_df <- slot(object, to)
-            if (!all(columns %in% colnames(from_df))) {
-              stop("All columns must come from the same table.", call. = FALSE)
-            }
-            df <- as.data.frame(object)
+              from <- match.arg(from, c("sets", "elements", "relations"))
+              to <- match.arg(to, c("sets", "elements", "relations"))
+              from_df <- slot(object, from)
+              to_df <- slot(object, to)
+              if (!all(columns %in% colnames(from_df))) {
+                  stop("All columns must come from the same table.",
+                       call. = FALSE)
+              }
+              df <- as.data.frame(object)
 
-            to_colnames <- colnames(to_df)
-            from_colnames <- colnames(from_df)
+              to_colnames <- colnames(to_df)
+              from_colnames <- colnames(from_df)
 
-            new_to <- unique(df[, c(to_colnames, columns), drop = FALSE])
-            new_from <- unique(from_df[, !from_colnames %in% columns,
-                                       drop = FALSE])
-            slot(object, to, check = FALSE) <- new_to
-            slot(object, from, check = FALSE) <- new_from
-            validObject(object)
-            object
+              new_to <- unique(df[, c(to_colnames, columns), drop = FALSE])
+              new_from <- unique(from_df[, !from_colnames %in% columns,
+                                         drop = FALSE])
+              slot(object, to, check = FALSE) <- new_to
+              slot(object, from, check = FALSE) <- new_from
+              validObject(object)
+              object
           }
 )
