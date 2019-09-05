c07639316e3bfac413be67a007eb8b20f1aa9c85
llrs <lluis.revilla@gmail.com>
2019-09-05 17:53:57 +0200

    Remove code that I'm not sure it adds anything

--- R/xml.R
+++
@@ -1,9 +0,0 @@
-#' @importFrom XML xmlTreeParse
-#' @examples
-#' fl <- system.file("extdata", "Broad.xml", package="GSEABase")
-#' gss <- getXML(fl) # GeneSetCollection of 2 sets
-getXML <- function(x, ...) {
-    # GSEABAse::getBroadSets -> .fromXML
-    res <- XML::xmlTreeParse(x, useInternalNodes = TRUE, ...)
-    # browser()
-}
