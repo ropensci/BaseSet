b00c2b439b0e40a8fb71e40390790e9bca6c68dc
Lluís <lluis.revilla@gmail.com>
2020-09-01 11:29:49 +0200

    Fix mistakes detected by r-hub
    Also show some examples on the package help page

--- R/basesets-package.R
+++ R/basesets-package.R
@@ -1,3 +1,8 @@
 #' @details
 #' It provides a class [`TidySet`] with methods to do operations with sets.
+#' @examples
+#' set <- list("A" = letters[1:5], "B" = letters[4:7])
+#' TS <- tidySet(set)
+#' cardinality(TS)
+#' intersection(TS, c("A", "B"))
 "_PACKAGE"
