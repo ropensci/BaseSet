a567ea02dbabb27e5c3b9aa3236f6df2a6953f37
Lluís <lluis.revilla@gmail.com>
2023-07-03 00:57:43 +0200

    Ensure the files work well

--- tests/testthat/test-obo.R
+++ tests/testthat/test-obo.R
@@ -1,14 +1,14 @@
 test_that("getOBO works", {
     skip_if_not_installed("GSEABase")
-    fl <- system.file("extdata", "goslim_plant.obo", package = "GSEABase")
+    fl <- system.file("extdata", "goslim_plant.obo", package = "GSEABase",
+                      mustWork = TRUE)
     y <- getOBO(fl)
     expect_true(is(y, "TidySet"))
 })
 
 test_that("getGAF works", {
     fl <- system.file("extdata", "go_human_rna_valid_subset.gaf",
-        package = "BaseSet"
-    )
+        package = "BaseSet", mustWork = TRUE)
     y <- getGAF(fl)
     expect_true(is(y, "TidySet"))
 })
