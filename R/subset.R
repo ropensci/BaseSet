8d8322a35717eccd2fd91ce0ccbd9a4afeabb0cc
Lluís <lluis.revilla@gmail.com>
2023-07-03 00:59:49 +0200

    Several upgrades to basic methods

---
+++ R/subset.R
@@ -0,0 +1,160 @@
+# From: https://stackoverflow.com/a/10961998
+## $ ####
+#' @export
+setMethod("$", "TidySet",
+          function(x, name) {
+            if (name %in% colnames(relations(x))) {
+              return(slot(x, "relations")[[name]])
+            }
+            if (name %in% colnames(sets(x))) {
+              return(slot(x, "sets")[[name]])
+            }
+            if (name %in% colnames(elements(x))) {
+              return(slot(x, "elements")[[name]])
+            }
+            NULL
+          })
+
+#' @export
+setMethod("$<-", "TidySet",
+          function(x, name, value) {
+            w <- which(length(value) == dim(x))
+            if (length(w) == 0) {
+              w <- 2
+              value <- rep(value, nRelations(x))
+            } else if (length(w) == 2 && 2 %in% w) {
+              # If multiple slots match assign to relations
+              warning("Matching multiple slots. Assigning value to relations.",
+                      call. = FALSE)
+              w <- 2
+            } else if (length(w) == 2) {
+              warning("Matching multiple slots. Randomly assigning the value.",
+                      call. = FALSE)
+              w <- sample(w, 1)
+            }
+
+            if (w == 1) {
+              elements(x)[[name]] <- value
+            } else if (w == 2) {
+              relations(x)[[name]] <- value
+            } else if (w == 3) {
+              sets(x)[[name]] <- value
+            }
+
+            x
+          })
+
+
+## [ ####
+#' @examples
+#' TS[1, "elements"]
+#' TS[, "sets", "type"]
+#' TS[, "sets", c("type", "origin")]
+#' TS[, "sets"]
+#' @export
+setMethod("[", "TidySet",
+          function(x, i, j, k, ..., drop = TRUE) {
+            if (!missing(i) && is.character(i)) {
+              stop("TidySet does not accept characters as `i` index for `[`.",
+                   "\nDid you meant to use [[ instead?", call. = FALSE)
+            }
+
+            if (missing(j)) {
+              j <- "relations"
+            }
+            if (length(j) > 1 || is.na(j)) {
+              stop("j only accepts: 'elements', 'sets' and ' relations'")
+            }
+            j <- match.arg(j, c("elements", "sets", "relations"))
+            s <-  slot(x, j)
+            if (missing(k)) {
+              k <- 1
+            }
+
+            k <- keep_columns(j, k)
+            if (missing(i)) {
+              s2 <- s[, k, ..., drop = FALSE]
+            } else {
+              s2 <- s[i, k, ..., drop = FALSE]
+              rownames(s2) <- NULL
+            }
+            slot(x, j) <- s2
+            droplevels(x, elements = drop, sets = drop, relations = drop)
+          })
+
+#' @examples
+#' TS[, "sets", "origin"] <- "abDT"
+#' TS[1, "sets", "sets"] <- "Abcd"
+#' @export
+setMethod("[<-", "TidySet",
+          function(x, i, j, k, ..., value) {
+            if (missing(j)) {
+              j <- "relations"
+            }
+            j <- match.arg(j, c("elements", "sets", "relations"))
+            s <-  slot(x, j)
+            if (missing(k)) {
+              k <- 1
+            }
+
+            s[i, k, ...] <- value
+            slot(x, j) <- s
+            drop <- TRUE
+            droplevels(x, elements = drop, sets = drop, relations = drop)
+          })
+
+## [[ ####
+#' @export
+#' @examples
+#' TS[["A"]]
+#' TS[["A"]]
+setMethod("[[", "TidySet",
+          function(x, i, j, ..., exact =TRUE) {
+            i <- unique(i)
+            i <- i[!is.na(i)]
+            if (length(i) > 1) {
+              stop("Trying to extract more than one set.", call. = FALSE)
+            }
+            stopifnot(rlang::is_logical(exact))
+            if (missing(j)) {
+              j <- 1
+            }
+            j <- keep_columns("sets", j)
+            ns <- nSets(x)
+            logical_i <- is.logical(i) && length(i) > ns
+            numeric_i <- is.numeric(i) && max(i, na.rm = TRUE) > ns
+            if ( logical_i | numeric_i) {
+              stop("Sets requested not available.")
+            }
+            nams <- name_sets(x)
+            if (is.character(i)) {
+              nsi <- i
+            } else if (is.character(i) && !exact) {
+              nsi <- pmatch(i, table = nams)
+            } else {
+              nsi <- nams[i]
+            }
+            namsi <- match(nsi, nams)
+            x[namsi, "sets", j, drop = TRUE]
+          })
+
+keep_columns <- function(j, k) {
+  if (is.numeric(k) && j == "relations") {
+    return(unique(c(1:3, k)))
+  } else if (is.numeric(k)) {
+    return(unique(c(1, k)))
+  }
+  if (j == "relations") {
+    return(unique(c("elements", "sets", "fuzzy", k)))
+  } else if (j == "sets") {
+    return(unique(c("sets", k)))
+  } else if (j == "elements") {
+    return(unique(c("elements", k)))
+  }
+
+  if (is.logical(k) && j == "relations") {
+    return(c(TRUE, TRUE, TRUE, k))
+  } else {
+    return(c(TRUE, k))
+  }
+}
---
+++ tests/testthat/test-subset.R
@@ -0,0 +1,80 @@
+## $ ####
+test_that("$ works in TidySet class", {
+  TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+  expect_length(TS$elements, 6)
+  expect_null(TS$adafd)
+})
+
+test_that("$<- works in TidySet class", {
+  TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+  expect_length(TS$elements, 6)
+  expect_warning(TS$adafd <- LETTERS[1:6])
+  expect_equal(TS$adafd, LETTERS[1:6])
+  expect_equal(ncol(relations(TS)), 4)
+
+  TS$adafd <- LETTERS[1:2]
+  expect_equal(TS$adafd, LETTERS[1:6])
+  expect_equal(sets(TS)$adafd, LETTERS[1:2])
+
+})
+
+## [ ####
+test_that("[i, j, k] subset works in TidySet class", {
+  TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+
+  expect_warning(TS$abcd <- sample(c("ha", "he"), size = 6, replace = TRUE))
+
+  expect_error(TS[, c(TRUE, FALSE, TRUE)], "j only accepts:")
+
+  out <- TS[1,"elements", ]
+  expect_s4_class(out, "TidySet")
+  expect_equal(nElements(out), 1)
+
+  out_v <- TS[c(1, 2, 2), "relations"]
+  # Six relations even if the rows are duplicated!
+  expect_equal(nRelations(out_v), 2)
+  expect_equal(nElements(out_v), 2)
+
+  expect_error(TS[c("A", "B", "B"), ])
+})
+
+test_that("[i] subset works in TidySet class", {
+  TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+  out1 <- TS[c(1, 2, 2)]
+  expect_equal(nRelations(out1), nRelations(TS[c(1, 2)]))
+  out <- TS[c(1, NA, 2)]
+  # NAs are ignored
+  expect_equal(TS[c(1, 2)], out)
+  # Watch out for the recycling vector rule!
+  out_l <- TS[c(TRUE, TRUE)]
+  expect_equal(out_l, TS)
+})
+
+## [[ ####
+test_that("[[i]] subset works in TidySet class", {
+  TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+  expect_equal(TS[[1]], TS[["A"]])
+})
+
+# test_that("[i, j]<- subset works in TidySet class", {
+#   TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+#   out <- TS[1, c("elements", "sets", "fuzzy")]
+#   expect_equal(dim(relations(out)), dim(relations(TS[1, ])))
+#   expect_error(TS[1, TS$elements], "undefined columns selected")
+# })
+#
+# test_that("[i]<- subset works in TidySet class", {
+#   TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+#   out <- TS[c(1, 2, 2)]
+#   out <- TS[c(1, NA, 2)]
+#   expect_equal(dim(relations(out)), dim(relations(TS[1, ])))
+#   expect_error(TS[1, TS$elements], "undefined columns selected")
+# })
+#
+# test_that("[[i]]<- subset works in TidySet class", {
+#   TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+#   out <- TS[c(1, 2, 2)]
+#   out <- TS[c(1, NA, 2)]
+#   expect_equal(dim(relations(out)), dim(relations(TS[1, ])))
+#   expect_error(TS[1, TS$elements], "undefined columns selected")
+# })
