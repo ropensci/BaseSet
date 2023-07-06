d5d2e54dee40b41b661b3614d3ea3bc0d5a9c954
Lluís <lluis.revilla@gmail.com>
2023-07-06 22:37:01 +0200

    Improve the combine method and document it

--- R/c.R
+++ R/c.R
@@ -1,11 +1,40 @@
 # Allow to merge TidySets
+#' Combine Values into a Vector or List
+#'
+#' This method combines TidySets.
+#' It only works if the first element is a TidySet.
+#' @param x A TidySet object.
+#' @param ... Objects to be concatenated. All NULL entries are dropped.
 #' @export
+#' @examples
+#' TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+#' TS2 <- c(TS, data.frame(sets = "C", elements = "gg"))
 setMethod("c", "TidySet",
           function(x, ...) {
-            dc <- lapply(list(x, ...), as.data.frame)
-            m <- function(x, y) {
-              merge(x, y, all = TRUE, sort = FALSE)
-            }
-            r <- Reduce(m, dc)
-            tidySet(r)
+              l <- list(x, ...)
+              null <- vapply(l, is.null, FUN.VALUE = logical(1))
+
+              dc <- lapply(l[!null], function(x){
+                  as.data.frame(tidySet(x))
+              })
+              # browser()
+              m <- function(x, y) {
+                  merge(x, y, all = TRUE, sort = FALSE)
+              }
+              r <- Reduce(m, dc)
+
+              missing_fuzzy <- is.na(r$fuzzy)
+              if (any(missing_fuzzy)) {
+                  new_sets <- r$sets[missing_fuzzy]
+                  new_elements <- r$elements[missing_fuzzy]
+                  if (all(is.na(new_sets)) && all(is.na(new_elements))) {
+                      warning("Some data might be lost.")
+                  }
+                  TS <- tidySet(r[!missing_fuzzy, , drop = FALSE])
+                  TS <- add_sets_internal(TS, new_sets)
+                  TS <- add_elements_internal(TS, new_elements)
+                  validObject(TS)
+                  return(TS)
+              }
+              tidySet(r)
           })
--- tests/testthat/test-c.R
+++ tests/testthat/test-c.R
@@ -9,3 +9,18 @@ test_that("c works", {
   expect_equal(nSets(TS3)/2, nSets(TS))
   expect_equal(nElements(TS3), nElements(TS))
 })
+
+test_that("c works with other types", {
+    TS <- tidySet(list(A = letters[1:5], B = letters[6]))
+    TS2 <- c(TS, data.frame(sets = "C", elements = "gg"))
+    expect_equal(dim(TS2), dim(TS) + c(1, 1, 1))
+
+    TS2 <- c(TS, list(C = "gg"))
+    expect_equal(dim(TS2), dim(TS) + c(1, 1, 1))
+
+    expect_error(c(TS, list(a = "C", fuzzy = NA)))
+    TS3 <- c(TS, NULL, list(C = "gg"))
+    expect_equal(TS2, TS3)
+    TS4 <- c(TS, NULL, list(C = "gg"), list(D = "ge"))
+    expect_equal(dim(TS4), c(Elements = 8, Relations = 8, Sets = 4))
+})
