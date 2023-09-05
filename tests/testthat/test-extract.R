dce677ec07e4544bcc4d51dd98ebbc922296a26b
Lluís <lluis.revilla@gmail.com>
2023-09-05 20:08:33 +0200

    Dummy commit to check hooks

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -86,6 +86,7 @@ test_that("[ with names work when they are factors", {
     TS[c("b", "c"), "elements", "NEW"] <- TRUE
     # A new element c with no relation but property NEW should appear.
     expect_equal(nElements(TS), 5)
+
 })
 
 test_that("add_column works as [<-", {
